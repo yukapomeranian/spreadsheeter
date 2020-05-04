098d4ee429587388f8067ca43c2a45db3608e9bf
yukapomeranian <kadokura00000@gmail.com>
2020-05-04 23:28:56 +0900

    set up deploy for GitHub pages.

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,3 +63,13 @@ export default {
     ignoreNotFoundWarnings: true
   }
 };
+
+// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: "/spreadsheeter/"
+  }
+} : {}
+export default {
+  ...routerBase
+}
