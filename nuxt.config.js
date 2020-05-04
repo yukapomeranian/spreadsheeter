efbaed9ab2a14e835106b710d09bf6c02c62b3ff
yukapomeranian <kadokura00000@gmail.com>
2020-05-04 16:46:09 +0900

    update nuxt.config.js to lintfix on dev mode.

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,17 +43,21 @@ export default {
   /*
   ** Build configuration
   */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    // extend (config, ctx) {
-    // }
+  extend(config, ctx) {
+    // Run ESLint on save
+    if (ctx.isDev && ctx.isClient) {
+      config.module.rules.push({
+        enforce: "pre",
+        test: /\.(js|ts|vue)$/,
+        loader: "eslint-loader",
+        exclude: /(node_modules)/
+      });
+    }
   },
   typescript: {
-    // typeCheck: {
-    //  eslint: true
-    // },
+    typeCheck: {
+      eslint: true
+    },
     ignoreNotFoundWarnings: true
   }
 };
